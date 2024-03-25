799605981c2030edf9e77602097d484ce288f2dc
Preetam Pati <preetam6teen@gmail.com>
2024-03-26 02:46:00 +0530

    Added Server templates - Preetam Pati

---
+++ LVCS_Server/LVCS_Hub/Server/Assets/__init__.py
@@ -0,0 +1,2 @@
+
+# To be treated as a common entry point for all assets 
---
+++ LVCS_Server/LVCS_Hub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LvcsHubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LVCS_Hub'
