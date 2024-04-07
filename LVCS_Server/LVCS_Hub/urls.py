5660bfa49be58de148d2b9f81ef4688a4467eb4a
Preetam Pati <preetam6teen@gmail.com>
2024-04-07 06:44:22 +0530

    Added config utils - Preetam Pati

--- LVCS_Server/LVCS_Hub/urls.py
+++ LVCS_Server/LVCS_Hub/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("ensure", views.ensure, name="ensure"),
+    path("config", views.config, name="config"),
     path("init", views.init, name="init"),
     path("add", views.add, name="add"),
     path("commit", views.commit, name="commit"),
